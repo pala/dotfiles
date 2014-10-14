9abb429243472492acf68506cfcc7ec29202d2ac
pala <ztpala@gmail.com>
2014-10-14 10:36:17 -0400

    Set editor to Atom

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias reload!='. ~/.zshrc'
 alias dotfiles="$EDITOR ~/.dotfiles"
 alias blog="$EDITOR $PROJECTS/pala.github.com"
 alias jkw='jekyll serve -w'
+alias xcclear="rm -rf ~/Library/Developer/Xcode/DerivedData/*"
