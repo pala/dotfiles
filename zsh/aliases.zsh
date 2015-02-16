b95534e340b35c07a62f86981368a7be5dfbc076
Tao Zhang <ztpala@gmail.com>
2015-02-16 09:31:58 -0500

    Use option-cmd-k to clean build folder

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,3 @@ alias reload!='. ~/.zshrc'
 alias dotfiles="$EDITOR ~/.dotfiles"
 alias blog="$EDITOR $PROJECTS/pala.github.io"
 alias jkw='jekyll serve -w'
-alias xcclear="rm -rf ~/Library/Developer/Xcode/DerivedData/*"
